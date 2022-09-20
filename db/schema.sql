67725f8a2d37c4bc2f4b85f25e017bf66d8fa8fb
Mostafa Maki <mostafa_m9@yahoo.com>
2022-09-19 20:53:57 -0700

    added schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS registrar_db;
+CREATE DATABASE registrar_db;
+
+USE registrar_db;
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  department_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INT NOT NULL
+);
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT NOT NULL
+);
