5091d93e32fb512fd55b8d5b0661da82e74bf295
Mostafa Maki <mostafa_m9@yahoo.com>
2022-09-20 10:17:19 -0700

    added inquirer and set up primary and foreign keys in schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,34 @@ CREATE DATABASE registrar_db;
 USE registrar_db;
 
 CREATE TABLE department (
+  -- this id needs to be a primary that links to department_id in roles
   id INT NOT NULL,
-  department_name VARCHAR(30) NOT NULL
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
-CREATE TABLE role (
-  id INT NOT NULL,
+CREATE TABLE roles (
+  -- this id needs to be primary to link with employee table
+  ID INT NOT NULL,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL NOT NULL,
-  department_id INT NOT NULL
+  --foreign key connected to to department id
+  department_id INT NOT NULL,
+  PRIMARY KEY (ID),
+  FOREIGN KEY(department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
 CREATE TABLE employee (
   id INT NOT NULL,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
+  -- foreign key coming from roles table
   role_id INT NOT NULL,
-  manager_id INT NOT NULL
+  -- ? manager id needs to be linked somehow to employee id based on role
+  manager_id INT NOT NULL,
+  FOREIGN KEY (role_id)
+  REFERENCES roles(ID)
+  ON DELETE SET NULL
 );
--- server.js
+++ server.js
@@ -10,3 +10,32 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // see folder 11 under mySQL
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'R0JmN1mf3l',
+      database: 'classlist_db'
+    },
+    // I think here
+    console.log(`Connected to the classlist_db database.`)
+);
+
+// Query database
+db.query('SELECT * FROM students', function (err, results) {
+    //or here
+    console.log(results);
+});
+
+// Default response for any other request (Not Found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is Live at ${PORT}`);
+});
